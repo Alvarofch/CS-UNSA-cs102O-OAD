73dd82a4a0134cc1d007c63c24b31208fb1f18c6
Alvaro Figueroa Chacon <alvarofch10@gmail.com>
2013-05-23 21:29:37 -0500

    Avanzado en clases el miercoles

--- MainCell.h
+++ MainCell.h
@@ -11,12 +11,13 @@ class MainCell
         cellValue evalue(){/**Aca va el codigo para obtener el valor que contiene una celda**/
                             //return mCellValue;
                             }
+        //update();
 
     private:
         cellValue mCellValue; /**Miembro de un bojeto celda que guarda el valor que contiene dicha celda**/
-      //  string strCellExpression;
+        string strCellExpression;
         Nodo* ptrNodeExpression;
-        //vector <*MainCell> listDependentCell;
+        vector <*MainCell> listDependentCell;
 
 };
 
--- Matrix.h
+++ Matrix.h
@@ -2,13 +2,26 @@
 #define MATRIX_H
 #include "MainCell.h"
 
+template <typename T>
+
+//Punteros a una clase IUpdate(no olvidar crear)
+//Crear una clase visualizadorMatrix
+
 class Matrix
 {
     private:
-
+        typedef vector <T> rowType;
+        vector <rowType> mMatrix;
     public:
         Matrix();
         virtual ~Matrix();
+        //void insert();
+        void set(size_t row,size_t col,T value){/** verificar tamaño
+                                                insertar un valor
+                                                actualizar**/};
+        T get(size_t row,size_t col);//revisar c++11
+        void resizeHor(size_t nCols);
+        void resizeVer(size_t nRows);
 };
 
 #endif // MATRIX_H
